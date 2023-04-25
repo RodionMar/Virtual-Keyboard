769d478a50592c4962964a6aebc7615dfb138155
RodionMar <rodionmarmura@gmail.com>
2023-04-25 09:03:54 +0300

    feat: create template for virtual keyboard

---
+++ src/js/keyboard-template.js
@@ -0,0 +1,505 @@
+export default function keyBoardTemplate() {
+  const backQuote = '`';
+  return `
+    <div class=”keyboard”>
+    <div class="keyboard__first-row">
+    <div class="keyboard__key keyboard__key--back-quote">
+      <div class="key__rus">
+        <div class="lower-case">ё</div>
+        <div class="upper-case">Ё</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">${backQuote}</div>
+        <div class="upper-case">~</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-one">
+      <div class="key__rus">
+        <div class="lower-case">1</div>
+        <div class="upper-case">!</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">1</div>
+        <div class="upper-case">!</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-two">
+      <div class="key__rus">
+        <div class="lower-case">2</div>
+        <div class="upper-case">"</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">2</div>
+        <div class="upper-case">@</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-three">
+      <div class="key__rus">
+        <div class="lower-case">3</div>
+        <div class="upper-case">№</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">3</div>
+        <div class="upper-case">#</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-four">
+      <div class="key__rus">
+        <div class="lower-case">4</div>
+        <div class="upper-case">;</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">4</div>
+        <div class="upper-case">$</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-five">
+      <div class="key__rus">
+        <div class="lower-case">5</div>
+        <div class="upper-case">%</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">5</div>
+        <div class="upper-case">%</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-six">
+      <div class="key__rus">
+        <div class="lower-case">6</div>
+        <div class="upper-case">:</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">6</div>
+        <div class="upper-case">^</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-seven">
+      <div class="key__rus">
+        <div class="lower-case">7</div>
+        <div class="upper-case">?</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">7</div>
+        <div class="upper-case">&</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-eight">
+      <div class="key__rus">
+        <div class="lower-case">8</div>
+        <div class="upper-case">*</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">8</div>
+        <div class="upper-case">*</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-nine">
+      <div class="key__rus">
+        <div class="lower-case">9</div>
+        <div class="upper-case">(</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">9</div>
+        <div class="upper-case">(</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--digit-zero">
+      <div class="key__rus">
+        <div class="lower-case">0</div>
+        <div class="upper-case">)</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">0</div>
+        <div class="upper-case">)</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--minus">
+      <div class="key__rus">
+        <div class="lower-case">-</div>
+        <div class="upper-case">_</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">-</div>
+        <div class="upper-case">_</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--equal">
+      <div class="key__rus">
+        <div class="lower-case">=</div>
+        <div class="upper-case">+</div>
+      </div>
+      <div class="key__eng">
+        <div class="lower-case">=</div>
+        <div class="upper-case">+</div>
+      </div>
+    </div>
+    <div class="keyboard__key keyboard__key--backspace">Backspace
+    </div>
+ </div>
+      <div class=”keyboard__second-row″>
+        <div class="keyboard__key keyboard__key--tab">Tab</div>
+        <div class="keyboard__key keyboard__key--keyQ">
+          <div class="key__rus">
+            <div class="lower-case">й</div>
+            <div class="upper-case">Й</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">q</div>
+            <div class="upper-case">Q</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyW">
+          <div class="key__rus">
+            <div class="lower-case">ц</div>
+            <div class="upper-case">Ц</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">w</div>
+            <div class="upper-case">W</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyE">
+          <div class="key__rus">
+            <div class="lower-case">у</div>
+            <div class="upper-case">У</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">e</div>
+            <div class="upper-case">E</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyR">
+        <div class="key__rus">
+            <div class="lower-case">к</div>
+            <div class="upper-case">К</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">r</div>
+            <div class="upper-case">R</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyT">
+          <div class="key__rus">
+            <div class="lower-case">е</div>
+            <div class="upper-case">Е</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">t</div>
+            <div class="upper-case">T</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyY">
+          <div class="key__rus">
+            <div class="lower-case">н</div>
+            <div class="upper-case">Н</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">y</div>
+            <div class="upper-case">Y</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyU">
+          <div class="key__rus">
+            <div class="lower-case">г</div>
+            <div class="upper-case">Г</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">u</div>
+            <div class="upper-case">U</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyI">
+          <div class="key__rus">
+            <div class="lower-case">ш</div>
+            <div class="upper-case">Ш</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">i</div>
+            <div class="upper-case">I</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyO">
+          <div class="key__rus">
+            <div class="lower-case">щ</div>
+            <div class="upper-case">Щ</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">o</div>
+            <div class="upper-case">O</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyP">
+        <div class="key__rus">
+            <div class="lower-case">з</div>
+            <div class="upper-case">З</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">p</div>
+            <div class="upper-case">P</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--left-square-bracket">
+          <div class="key__rus">
+            <div class="lower-case">х</div>
+            <div class="upper-case">Х</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">[</div>
+            <div class="upper-case">{</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--right-square-bracket">
+          <div class="key__rus">
+            <div class="lower-case">ъ</div>
+            <div class="upper-case">Ъ</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">]</div>
+            <div class="upper-case">}</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--back-slash">
+          <div class="key__rus">
+            <div class="lower-case">\</div>
+            <div class="upper-case">/</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">\</div>
+            <div class="upper-case">|</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--del">Del</div>
+      </div>
+      <div class=”keyboard__third-row″>
+        <div class="keyboard__key keyboard__key--caps-lock">CapsLock</div>
+        <div class="keyboard__key keyboard__key--keyA">
+          <div class="key__rus">
+            <div class="lower-case">ф</div>
+            <div class="upper-case">Ф</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">a</div>
+            <div class="upper-case">A</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyS">
+          <div class="key__rus">
+            <div class="lower-case">ы</div>
+            <div class="upper-case">Ы</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">s</div>
+            <div class="upper-case">S</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyD">
+          <div class="key__rus">
+            <div class="lower-case">в</div>
+            <div class="upper-case">В</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">d</div>
+            <div class="upper-case">D</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyF">
+          <div class="key__rus">
+            <div class="lower-case">а</div>
+            <div class="upper-case">а</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">f</div>
+            <div class="upper-case">F</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyG">
+          <div class="key__rus">
+            <div class="lower-case">п</div>
+            <div class="upper-case">П</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">g</div>
+            <div class="upper-case">G</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyH">
+          <div class="key__rus">
+            <div class="lower-case">р</div>
+            <div class="upper-case">Р</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">h</div>
+            <div class="upper-case">H</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyJ">
+          <div class="key__rus">
+            <div class="lower-case">о</div>
+            <div class="upper-case">О</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">j</div>
+            <div class="upper-case">J</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyK">
+          <div class="key__rus">
+            <div class="lower-case">л</div>
+            <div class="upper-case">Л</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">k</div>
+            <div class="upper-case">K</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyL">
+          <div class="key__rus">
+            <div class="lower-case">д</div>
+            <div class="upper-case">Д</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">l</div>
+            <div class="upper-case">L</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--semicolon">
+          <div class="key__rus">
+            <div class="lower-case">ж</div>
+            <div class="upper-case">Ж</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">;</div>
+            <div class="upper-case">:</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--quote">
+          <div class="key__rus">
+            <div class="lower-case">э</div>
+            <div class="upper-case">Э</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">'</div>
+            <div class="upper-case">"</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--enter">Enter</div>
+      </div>
+      <div class=”keyboard__fourth-row″>
+        <div class="keyboard__key keyboard__key--left-shift">Shift</div>
+        <div class="keyboard__key keyboard__key--keyZ">
+          <div class="key__rus">
+            <div class="lower-case">я</div>
+            <div class="upper-case">Я</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">z</div>
+            <div class="upper-case">Z</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyX">
+          <div class="key__rus">
+            <div class="lower-case">ч</div>
+            <div class="upper-case">Ч</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">x</div>
+            <div class="upper-case">X</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyC">
+          <div class="key__rus">
+            <div class="lower-case">с</div>
+            <div class="upper-case">С</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">c</div>
+            <div class="upper-case">C</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyV">
+          <div class="key__rus">
+            <div class="lower-case">м</div>
+            <div class="upper-case">М</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">v</div>
+            <div class="upper-case">V</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyB">
+          <div class="key__rus">
+            <div class="lower-case">и</div>
+            <div class="upper-case">И</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">b</div>
+            <div class="upper-case">B</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyN">
+          <div class="key__rus">
+            <div class="lower-case">т</div>
+            <div class="upper-case">Т</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">n</div>
+            <div class="upper-case">N</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--keyM">
+          <div class="key__rus">
+            <div class="lower-case">ь</div>
+            <div class="upper-case">Ь</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">m</div>
+            <div class="upper-case">M</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--comma">
+          <div class="key__rus">
+            <div class="lower-case">б</div>
+            <div class="upper-case">Б</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">,</div>
+            <div class="upper-case"><</div>
+          </div>
+        </div>
+        <div class="keyboard__key keyboard__key--dot">
+          <div class="key__rus">
+            <div class="lower-case">ю</div>
+            <div class="upper-case">Ю</div>
+          </div>
+          <div class="key__eng">
+            <div class="lower-case">.</div>
+            <div class="upper-case">></div>
+          </div>
+         </div>
+         <div class="keyboard__key keyboard__key--slash">
+           <div class="key__rus">
+             <div class="lower-case">.</div>
+             <div class="upper-case">,</div>
+           </div>
+           <div class="key__eng">
+             <div class="lower-case">/</div>
+             <div class="upper-case">?</div>
+           </div>
+        </div>
+        <div class="keyboard__key keyboard__key--top-arrow">▲</div>
+        <div class="keyboard__key keyboard__key--right-shift">Shift</div>
+      </div>
+      <div class=”keyboard__fifth-row″>
+        <div class="keyboard__key keyboard__key--left-ctrl">Ctrl</div>
+        <div class="keyboard__key keyboard__key--win">Win</div>
+        <div class="keyboard__key keyboard__key--left-alt">Alt</div>
+        <div class="keyboard__key keyboard__key--space"></div>
+        <div class="keyboard__key keyboard__key--right-alt">Alt</div>
+        <div class="keyboard__key keyboard__key--left-arrow">◄</div>
+        <div class="keyboard__key keyboard__key--bottom-arrow">▼</div>
+        <div class="keyboard__key keyboard__key--right-arrow">►</div>
+        <div class="keyboard__key keyboard__key--right-ctrl">Ctrl</div>
+      </div>
+</div>
+  `
+}
